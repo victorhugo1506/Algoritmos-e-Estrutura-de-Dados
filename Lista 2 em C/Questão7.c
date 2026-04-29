97607639b49680c70a9ecd9d14ec42080c40cda7
victorhugo1506 <hugo.victor@aluno.ufca.edu.br>
2026-04-29 17:13:51 -0300

    docs: adiciona a questão 7

---
+++ Lista 2 em C/Questão7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int V[] = {2,1,9,7,6,3,9,4,2,6,1,3};
+    int n = 12;
+    int k = 0;
+    int achou = 0;
+
+    printf("Digite o valor de k: ");
+    scanf("%d", &k);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j <= i + k && j < n; j++) {
+            if (V[i] == V[j]) {
+                printf("Sim, o %d nas posicoes %d e %d\n", V[i], i, j);
+                achou = 1;
+            }
+        }
+    }
+
+    if (!achou){
+        printf("Nao existem elementos repetidos dentro da distancia %d\n", k);
+    }
+
+    return 0;
+}
