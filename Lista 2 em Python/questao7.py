43a1e5afa46305cb75d13386faa944eb1fb9fd82
victorhugo1506 <hugo.victor@aluno.ufca.edu.br>
2026-04-29 17:39:32 -0300

    docs: adiciona as questões da lista 2 em python

---
+++ Lista 2 em Python/questao7.py
@@ -0,0 +1,14 @@
+V = [2, 1, 9, 7, 6, 3, 9, 4, 2, 6, 1, 3]
+n = len(V)
+achou = False
+
+k = int(input("Digite o valor de k: "))
+
+for i in range(n):
+    for j in range(i + 1, min(i + k + 1, n)):
+        if V[i] == V[j]:
+            print(f"Sim, o {V[i]} nas posicoes {i} e {j}")
+            achou = True
+
+if not achou:
+    print(f"Nao existem elementos repetidos dentro da distancia {k}")
