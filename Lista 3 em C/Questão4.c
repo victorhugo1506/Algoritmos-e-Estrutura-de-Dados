bea6d940d58facc3852aebfbe7bebc6df6cde015
victorhugo1506 <hugo.victor@aluno.ufca.edu.br>
2026-05-05 17:02:20 -0300

    docs: adiciona as questões 4 e 5

---
+++ Lista 3 em C/Questão4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int V[9][9] = {
+        {3, 9, 4, 2, 4, 1, 8, 5, 1},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {5, 8, 2, 3, 9, 8, 4, 1, 7},
+        {8, 3, 4, 2, 3, 1, 3, 9, 4},
+        {3, 7, 2, 9, 4, 2, 1, 2, 3},
+        {7, 5, 3, 1, 2, 4, 5, 8, 2},
+        {4, 7, 3, 6, 6, 1, 9, 3, 2},
+        {1, 5, 3, 2, 9, 8, 7, 6, 5},
+        {3, 9, 4, 2, 4, 1, 8, 5, 10}
+    };
+    int contador = 0;
+    int numeroparaprocurar = 0;
+
+    printf("Digite um valor para ver se esta na matriz e se ele se repete: ");
+    scanf("%d",&numeroparaprocurar);
+
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            if(V[i][j]==numeroparaprocurar){
+                contador++;
+            }
+        }
+    }
+
+    if(contador > 1){
+        printf("%d eh repetido %d vezes", numeroparaprocurar, contador);
+    } else if(contador == 1){
+        printf("%d so aparece uma vez", numeroparaprocurar);
+    } else{
+        printf("%d nao aparece na matriz", numeroparaprocurar);
+    }
+
+    return 0;
+}
---
+++ Lista 3 em C/Questão5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int V[9][9] = {
+        {3, 9, 4, 2, 4, 1, 8, 5, 1},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {5, 8, 2, 3, 9, 8, 4, 1, 7},
+        {8, 3, 4, 2, 3, 1, 3, 9, 4},
+        {3, 7, 2, 9, 4, 2, 1, 2, 3},
+        {7, 5, 3, 1, 2, 4, 5, 8, 2},
+        {4, 7, 3, 6, 6, 1, 9, 3, 2},
+        {1, 5, 3, 2, 9, 8, 7, 6, 5},
+        {3, 9, 4, 2, 4, 1, 8, 5, 1}
+    };
+    int verificador = 0;
+    
+    for(int i = 0; i < 9; i++){
+        for(int j = i+1; j < 9; j++){
+            int linhasiguais = 1;
+            for(int k = 0; k < 9; k++){
+                if(V[i][k] != V[j][k]){
+                    linhasiguais = 0;
+                    break;
+                }
+            }
+            if(linhasiguais == 1){
+                verificador = 1;
+                printf("As linhas %d e %d sao iguais", i+1, j+1);
+            }
+        }
+    }
+
+    if(verificador == 0){
+        printf("A matriz nao possui linhas iguais");
+    }
+
+    return 0;
+}
